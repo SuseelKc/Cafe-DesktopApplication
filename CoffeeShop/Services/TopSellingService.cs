4d41fe0de85793dc4637d9b36f2d818a13e63f86
SuseelKc <sushil.np.khatri@gmail.com>
2024-01-10 18:11:55 +0545

    Top selling items display

---
+++ CoffeeShop/Services/TopSellingService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoffeeShop.Models;
+
+namespace CoffeeShop.Services
+{
+    public class TopSellingService
+    {
+
+        public static List<TopSellingItem> GetTopSellingItems(List<BillDetail> billDetails, Func<BillDetail, string> selector)
+        {
+            var topSellingItems = billDetails
+                .Where(b => !string.IsNullOrEmpty(selector(b)))
+                .GroupBy(selector)
+                .Select(g => new TopSellingItem
+                {
+                    Name = g.Key,
+                    QuantitySold = g.Sum(x => x.Qty)
+                })
+                .OrderByDescending(x => x.QuantitySold)
+                .Take(5)
+                .ToList();
+
+            return topSellingItems;
+        }
+    }
+}
